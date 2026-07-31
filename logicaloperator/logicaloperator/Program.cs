a4f72eaef2f936f95488ef921078cadd1b3c1459
Diksha Goyal <bansaldiksha1602@gmail.com>
2026-07-31 12:59:19 +0530

    Completed Day 06 - Logical Operators and renamed project folders

---
+++ logicaloperator/logicaloperator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicaloperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+            int marks = 70;
+
+            Console.WriteLine(age >= 18 || marks >= 60);
+            Console.WriteLine(age >= 18 && marks >= 70);
+
+
+            string degree = "MCA";
+
+            Console.WriteLine(degree == "BCA" || degree == "MCA");
+
+
+            bool isloggedIn = true;
+            Console.WriteLine(!isloggedIn);
+
+
+
+        }
+    }
+}
---
+++ miniproject-college admission checker/miniproject-college admission checker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniproject_college_admission_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            bool eligible = age >= 18 && marks >= 60;
+
+            Console.WriteLine("Admission Eligible: " + eligible);
+
+            Console.ReadLine();
+        }
+    }
+}
