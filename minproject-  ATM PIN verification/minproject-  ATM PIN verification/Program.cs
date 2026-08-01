ac1d8ca39ceb9fb93171c1a44123c320c98fee36
Diksha Goyal <bansaldiksha1602@gmail.com>
2026-08-01 16:59:11 +0530

    Added ATM PIN Verification MIni Project

---
+++ minproject-  ATM PIN verification/minproject-  ATM PIN verification/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minproject___ATM_PIN_verification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert Card (yes/no): ");
+            string card = Console.ReadLine();
+
+            Console.Write("Enter Pin: ");
+            string pin = Console.ReadLine();
+
+            if (card == "yes")
+            {
+                if (pin == "1234")
+                {
+                    Console.WriteLine("Withdrawal Allowed");
+                }
+                else
+                {
+                    Console.WriteLine("wrong pin");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Card not Inserted");
+            }
+
+            Console.WriteLine("Thankyou");
+        }
+    }
+}
