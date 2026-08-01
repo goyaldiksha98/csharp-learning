133bc6696f20f5c921a3195ba1dfe64382ded010
Diksha Goyal <bansaldiksha1602@gmail.com>
2026-08-01 21:10:30 +0530

    Added Loan Approval System Mini Project

---
+++ miniproject- loan approval system/miniproject- loan approval system/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniproject__loan_approval_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 21)
+            {
+
+                Console.WriteLine("Enter Salary");
+                int salary = Convert.ToInt32(Console.ReadLine());
+
+
+                if (salary >= 30000)
+                {
+                    Console.WriteLine("Loan Approved");
+                }
+                else
+                {
+                    Console.WriteLine("Low Salary");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Age Not Eligible");
+
+            }
+            
+            Console.WriteLine("Process Completed");
+
+        }
+    }
+}
