1c707ba1e7c537175bdb895c4bbc1f2207c7a7ca
Diksha Goyal <bansaldiksha1602@gmail.com>
2026-08-03 10:35:31 +0530

    Add ATM Menu System using Switch Statement

---
+++ miniproject- ATM Menu System/miniproject- ATM Menu System/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniproject__ATM_Menu_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("=============ATM MENU============");
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Exit");
+
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Your Balance is Rs. 50,000");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Withdraw Successful");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Amount Deposited Successfully");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thank You for Using Our ATM");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ switch statement/switch statement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 2;
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Red");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Green");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Blue");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        }
+    }
+}
